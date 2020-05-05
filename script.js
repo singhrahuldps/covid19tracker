d8773e40e0fe3ce8067fef75f6ceba0c3ad5142e
Rahul Singh <singhrahuldps@gmail.com>
2020-05-05 20:31:19 +0530

    fixed Iframe src load

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-var setHTMLButton = document.getElementById('setHtml');
+window.addEventListener('DOMContentLoaded', function(){
+    
+    var countryselect = document.querySelector('#countryselect');
+    var frame = document.querySelector('#mapframe');
+    
+    document.querySelector('#setFrameHTML').addEventListener('click', function(){
+        var newHTML= 'charts/singhrahuldps/BestFitBellCurve/' + countryselect.options[countryselect.selectedIndex].value + '.html';
+        //var linkHTML = 'https://raw.githack.com/singhrahuldps/covid19tracker/master/charts/singhrahuldps/BestFitBellCurve/' + countryselect.options[countryselect.selectedIndex].value + '.html';
+        //alert(linkHTML);
+        //downloadURI(linkHTML, newHTML);
+        frame.src = newHTML;
+    });
+});
 
-function setHTML(){
-    var countryselect = document.getElementById('countryselect');
-    var frame = document.getElementById('mapframe');
-    var newHTML= 'charts/singhrahuldps/BestFitBellCurve/' + countryselect.options[countryselect.selectedIndex].value + '.html';
-    frame.src = newHTML;
-}
-
-setHTMLButton.onclick(setHTML());
